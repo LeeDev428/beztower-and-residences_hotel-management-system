cba8cf69658a54590c2c46d5c7c462788f2a2b7d
Lee Torres <grafrafraftorres28@gmail.com>
2025-12-31 23:31:19 +0800

    Update 2025_12_31_152732_create_block_dates_table.php

--- database/migrations/2025_12_31_152732_create_block_dates_table.php
+++ database/migrations/2025_12_31_152732_create_block_dates_table.php
@@ -13,6 +13,11 @@ return new class extends Migration
     {
         Schema::create('block_dates', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('room_id')->constrained()->onDelete('cascade');
+            $table->date('start_date');
+            $table->date('end_date');
+            $table->string('reason');
+            $table->foreignId('created_by')->constrained('users')->onDelete('cascade');
             $table->timestamps();
         });
     }
