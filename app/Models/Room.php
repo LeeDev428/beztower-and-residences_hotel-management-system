a71c3e562b162d75b8b0c5767aebbbb87781a3b9
Lee Torres <grafrafraftorres28@gmail.com>
2025-12-31 23:51:59 +0800

    Update Room.php

--- app/Models/Room.php
+++ app/Models/Room.php
@@ -6,5 +6,41 @@ use Illuminate\Database\Eloquent\Model;
 
 class Room extends Model
 {
-    //
+    protected $fillable = [
+        'room_type_id',
+        'room_number',
+        'floor',
+        'description',
+        'status'
+    ];
+
+    public function roomType()
+    {
+        return $this->belongsTo(RoomType::class);
+    }
+
+    public function amenities()
+    {
+        return $this->belongsToMany(Amenity::class, 'room_amenities');
+    }
+
+    public function photos()
+    {
+        return $this->hasMany(RoomPhoto::class);
+    }
+
+    public function bookings()
+    {
+        return $this->hasMany(Booking::class);
+    }
+
+    public function housekeeping()
+    {
+        return $this->hasOne(Housekeeping::class)->latest();
+    }
+
+    public function blockDates()
+    {
+        return $this->hasMany(BlockDate::class);
+    }
 }
