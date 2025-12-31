1044d3e7578f390835e2f9b0175b687f79c48f3e
Lee Torres <grafrafraftorres28@gmail.com>
2025-12-31 23:31:17 +0800

    Update 2025_12_31_152729_create_room_photos_table.php

--- database/migrations/2025_12_31_152729_create_room_photos_table.php
+++ database/migrations/2025_12_31_152729_create_room_photos_table.php
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('room_photos', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('room_id')->constrained()->onDelete('cascade');
+            $table->string('photo_path');
+            $table->boolean('is_primary')->default(false);
             $table->timestamps();
         });
     }
