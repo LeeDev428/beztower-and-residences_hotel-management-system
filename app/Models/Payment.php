1f2ca0c08bb6428d864ed93fade28025019a6ad7
Lee Torres <grafrafraftorres28@gmail.com>
2025-12-31 23:56:32 +0800

    Update Payment.php

--- app/Models/Payment.php
+++ app/Models/Payment.php
@@ -6,5 +6,23 @@ use Illuminate\Database\Eloquent\Model;
 
 class Payment extends Model
 {
-    //
+    protected $fillable = [
+        'booking_id',
+        'payment_method',
+        'payment_reference',
+        'amount',
+        'payment_status',
+        'payment_date',
+        'qr_code_path'
+    ];
+
+    protected $casts = [
+        'amount' => 'decimal:2',
+        'payment_date' => 'datetime'
+    ];
+
+    public function booking()
+    {
+        return $this->belongsTo(Booking::class);
+    }
 }
