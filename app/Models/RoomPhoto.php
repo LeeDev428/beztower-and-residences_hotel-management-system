9deff586f82b4a1ddfa8e1061925e3cd44d265d7
Lee Torres <grafrafraftorres28@gmail.com>
2025-12-31 23:56:33 +0800

    Update RoomPhoto.php

--- app/Models/RoomPhoto.php
+++ app/Models/RoomPhoto.php
@@ -6,5 +6,18 @@ use Illuminate\Database\Eloquent\Model;
 
 class RoomPhoto extends Model
 {
-    //
+    protected $fillable = [
+        'room_id',
+        'photo_path',
+        'is_primary'
+    ];
+
+    protected $casts = [
+        'is_primary' => 'boolean'
+    ];
+
+    public function room()
+    {
+        return $this->belongsTo(Room::class);
+    }
 }
