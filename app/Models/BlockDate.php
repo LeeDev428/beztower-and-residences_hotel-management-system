0ef4153748e61d0a399f962f18cc73ac85c49bf8
Lee Torres <grafrafraftorres28@gmail.com>
2025-12-31 23:56:27 +0800

    Update BlockDate.php

--- app/Models/BlockDate.php
+++ app/Models/BlockDate.php
@@ -6,5 +6,26 @@ use Illuminate\Database\Eloquent\Model;
 
 class BlockDate extends Model
 {
-    //
+    protected $fillable = [
+        'room_id',
+        'start_date',
+        'end_date',
+        'reason',
+        'created_by'
+    ];
+
+    protected $casts = [
+        'start_date' => 'date',
+        'end_date' => 'date'
+    ];
+
+    public function room()
+    {
+        return $this->belongsTo(Room::class);
+    }
+
+    public function creator()
+    {
+        return $this->belongsTo(User::class, 'created_by');
+    }
 }
