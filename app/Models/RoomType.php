767c4115ac94bb3a23103d706074e38658bd80f8
Lee Torres <grafrafraftorres28@gmail.com>
2025-12-31 23:52:01 +0800

    Update RoomType.php

--- app/Models/RoomType.php
+++ app/Models/RoomType.php
@@ -6,5 +6,19 @@ use Illuminate\Database\Eloquent\Model;
 
 class RoomType extends Model
 {
-    //
+    protected $fillable = [
+        'name',
+        'description',
+        'base_price',
+        'max_guests'
+    ];
+
+    protected $casts = [
+        'base_price' => 'decimal:2'
+    ];
+
+    public function rooms()
+    {
+        return $this->hasMany(Room::class);
+    }
 }
