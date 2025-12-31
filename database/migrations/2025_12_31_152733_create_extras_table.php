9ea7e20a2d987e5abff7be0d0cce0d50d6cf0258
Lee Torres <grafrafraftorres28@gmail.com>
2025-12-31 23:31:21 +0800

    Update 2025_12_31_152733_create_extras_table.php

--- database/migrations/2025_12_31_152733_create_extras_table.php
+++ database/migrations/2025_12_31_152733_create_extras_table.php
@@ -13,6 +13,10 @@ return new class extends Migration
     {
         Schema::create('extras', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->text('description')->nullable();
+            $table->decimal('price', 10, 2);
+            $table->boolean('is_active')->default(true);
             $table->timestamps();
         });
     }
