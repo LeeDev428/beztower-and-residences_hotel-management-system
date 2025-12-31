a56b776c3cb0046c5c8be09b74cb6a2c25664514
Lee Torres <grafrafraftorres28@gmail.com>
2025-12-31 23:31:14 +0800

    Update 2025_12_31_152726_create_guests_table.php

--- database/migrations/2025_12_31_152726_create_guests_table.php
+++ database/migrations/2025_12_31_152726_create_guests_table.php
@@ -13,6 +13,14 @@ return new class extends Migration
     {
         Schema::create('guests', function (Blueprint $table) {
             $table->id();
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->string('email');
+            $table->string('phone');
+            $table->string('country')->nullable();
+            $table->text('address')->nullable();
+            $table->string('id_number')->nullable();
+            $table->text('preferences')->nullable();
             $table->timestamps();
         });
     }
