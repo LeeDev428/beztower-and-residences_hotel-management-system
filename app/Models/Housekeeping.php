03b881c4e7b2e87a283f8d874be7e2d715cb4d0d
Lee Torres <grafrafraftorres28@gmail.com>
2025-12-31 23:56:30 +0800

    Update Housekeeping.php

--- app/Models/Housekeeping.php
+++ app/Models/Housekeeping.php
@@ -6,5 +6,20 @@ use Illuminate\Database\Eloquent\Model;
 
 class Housekeeping extends Model
 {
-    //
+    protected $fillable = [
+        'room_id',
+        'status',
+        'assigned_to',
+        'notes',
+        'last_cleaned_at'
+    ];
+
+    protected $casts = [
+        'last_cleaned_at' => 'datetime'
+    ];
+
+    public function room()
+    {
+        return $this->belongsTo(Room::class);
+    }
 }
