a49f967b2af9dded1f37996d5fb1a5547fe5fd03
Lee Torres <grafrafraftorres28@gmail.com>
2026-01-01 14:00:14 +0800

    Update home.blade.php

--- resources/views/customer/home.blade.php
+++ resources/views/customer/home.blade.php
@@ -655,7 +655,7 @@
         </div>
         
         <ul class="nav-links">
-            <li><a href="#home">Home</a></li>
+            <li><a href="/">Home</a></li>
             <li><a href="#about">About</a></li>
             <li><a href="#rooms">Rooms</a></li>
             <li><a href="#services">Services</a></li>
