95eeeb46834373442a563972a8a5d6931424218d
Lee Torres <grafrafraftorres28@gmail.com>
2026-01-01 16:12:14 +0800

    Update Payment.php

--- app/Models/Payment.php
+++ app/Models/Payment.php
@@ -8,21 +8,33 @@ class Payment extends Model
 {
     protected $fillable = [
         'booking_id',
+        'payment_type',
         'payment_method',
         'payment_reference',
         'amount',
+        'percentage',
         'payment_status',
         'payment_date',
-        'qr_code_path'
+        'proof_of_payment',
+        'payment_notes',
+        'verified_at',
+        'verified_by'
     ];
 
     protected $casts = [
         'amount' => 'decimal:2',
-        'payment_date' => 'datetime'
+        'percentage' => 'decimal:2',
+        'payment_date' => 'datetime',
+        'verified_at' => 'datetime'
     ];
 
     public function booking()
     {
         return $this->belongsTo(Booking::class);
+    }
+
+    public function verifiedBy()
+    {
+        return $this->belongsTo(User::class, 'verified_by');
     }
 }
