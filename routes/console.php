83d1d07faf68478774ee89d05f392ff50aaacbb0
Lee Torres <grafrafraftorres28@gmail.com>
2026-01-01 23:03:03 +0800

    Update console.php

--- routes/console.php
+++ routes/console.php
@@ -2,7 +2,11 @@
 
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Schedule;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+// Schedule checkout reminders to be sent every day at 8:00 AM
+Schedule::command('bookings:send-checkout-reminders')->dailyAt('08:00');
