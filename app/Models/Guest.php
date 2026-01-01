6d9458d06f714997407954f4d3f423706a535ccc
Lee Torres <grafrafraftorres28@gmail.com>
2026-01-02 00:39:43 +0800

    Update Guest.php

--- app/Models/Guest.php
+++ app/Models/Guest.php
@@ -22,6 +22,11 @@ class Guest extends Model
         return $this->hasMany(Booking::class);
     }
 
+    public function getNameAttribute()
+    {
+        return $this->first_name . ' ' . $this->last_name;
+    }
+
     public function getFullNameAttribute()
     {
         return $this->first_name . ' ' . $this->last_name;
