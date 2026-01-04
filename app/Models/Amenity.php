0838f51ed28a1375a21a83cdac31c970753dade5
Lee Torres <grafrafraftorres28@gmail.com>
2026-01-04 18:15:48 +0800

    Update Amenity.php

--- app/Models/Amenity.php
+++ app/Models/Amenity.php
@@ -8,8 +8,45 @@ class Amenity extends Model
 {
     protected $fillable = [
         'name',
-        'icon'
+        'icon',
+        'archived_at'
     ];
+
+    protected $casts = [
+        'archived_at' => 'datetime'
+    ];
+
+    // Scope to get only active (non-archived) amenities
+    public function scopeActive($query)
+    {
+        return $query->whereNull('archived_at');
+    }
+
+    // Scope to get only archived amenities
+    public function scopeArchived($query)
+    {
+        return $query->whereNotNull('archived_at');
+    }
+
+    // Method to archive an amenity
+    public function archive()
+    {
+        $this->archived_at = now();
+        $this->save();
+    }
+
+    // Method to restore an archived amenity
+    public function restore()
+    {
+        $this->archived_at = null;
+        $this->save();
+    }
+
+    // Check if amenity is archived
+    public function isArchived()
+    {
+        return !is_null($this->archived_at);
+    }
 
     public function rooms()
     {
