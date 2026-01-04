aedeebf0d9c623ce7785b1ad84548bc4618121f6
Lee Torres <grafrafraftorres28@gmail.com>
2026-01-04 18:10:06 +0800

    Update home.blade.php

--- resources/views/customer/home.blade.php
+++ resources/views/customer/home.blade.php
@@ -749,7 +749,7 @@
         
         <div class="map-container">
             <iframe 
-                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3860.3836567891644!2d121.03553631484284!3d14.603754389788835!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3397b7c3e0e0e0e1%3A0x0!2zMTTCsDM2JzEzLjUiTiAxMjHCsDAyJzE4LjMiRQ!5e0!3m2!1sen!2sph!4v1704067200000!5m2!1sen!2sph" 
+                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3860.9!2d121.038!3d14.5986!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x0!2zMTTCsDM1JzU1LjAiTiAxMjHCsDAyJzE2LjgiRQ!5e0!3m2!1sen!2sph!4v1704067200000!5m2!1sen!2sph&q=205+F+Blumentritt+Street+Brgy+Pedro+Cruz+San+Juan+City+Philippines" 
                 allowfullscreen="" 
                 loading="lazy" 
                 referrerpolicy="no-referrer-when-downgrade">
