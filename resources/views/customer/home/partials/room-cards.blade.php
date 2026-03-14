bc3b36af6ea0ad77998e3b1cd4d421177e24315c
Lee Torres <grafrafraftorres28@gmail.com>
2026-03-14 21:03:50 +0800

    Update room-cards.blade.php

--- resources/views/customer/home/partials/room-cards.blade.php
+++ resources/views/customer/home/partials/room-cards.blade.php
@@ -7,9 +7,6 @@
                 <img src="https://via.placeholder.com/400x300/d4af37/2c2c2c?text={{ urlencode($room->roomType->name) }}" alt="{{ $room->roomType->name }}">
             @endif
             <div class="room-badge">{{ $room->status }}</div>
-            @if($room->discount_percentage > 0)
-                <div class="discount-badge">{{ $room->discount_percentage }}% OFF</div>
-            @endif
         </div>
         
         <div class="room-details">
@@ -34,16 +31,9 @@
             
             <div class="room-footer">
                 <div class="room-price">
-                    @if($room->discount_percentage > 0)
-                        <span class="price-label">From</span>
-                        <span class="original-price">₱{{ number_format($room->roomType->base_price, 2) }}</span>
-                        <span class="price-amount discounted-price">₱{{ number_format($room->roomType->base_price * (1 - $room->discount_percentage / 100), 2) }}</span>
-                        <span class="price-period">/night</span>
-                    @else
-                        <span class="price-label">From</span>
-                        <span class="price-amount">₱{{ number_format($room->roomType->base_price, 2) }}</span>
-                        <span class="price-period">/night</span>
-                    @endif
+                    <span class="price-label">From</span>
+                    <span class="price-amount">₱{{ number_format($room->roomType->base_price, 2) }}</span>
+                    <span class="price-period">/night</span>
                 </div>
                 <a href="{{ route('rooms.show', $room) }}" class="book-btn">Learn More</a>
             </div>
