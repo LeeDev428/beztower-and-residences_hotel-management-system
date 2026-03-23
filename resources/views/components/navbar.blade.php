ab60174cb4edb5baf5b930722aa73ea3100967fa
Lee Torres <grafrafraftorres28@gmail.com>
2026-03-24 00:11:08 +0800

    Update navbar.blade.php

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -7,7 +7,7 @@
     <ul class="nav-links">
         <li><a href="{{ route('home') }}">Home</a></li>
         <li><a href="{{ route('home') }}#about">About</a></li>
-        <li><a href="{{ route('rooms.index') }}">Rooms</a></li>
+        <li><a href="{{ route('home') }}#rooms">Rooms</a></li>
         <li><a href="{{ route('home') }}#services">Services</a></li>
         <li><a href="{{ route('home') }}#contact">Contact</a></li>
     </ul>
