9aed2e3909048b7ffbaeff065188c0b7b56ad06a
Lee Torres <grafrafraftorres28@gmail.com>
2026-03-27 09:14:56 +0800

    Update checkout-reminder.blade.php

--- resources/views/emails/checkout-reminder.blade.php
+++ resources/views/emails/checkout-reminder.blade.php
@@ -88,7 +88,8 @@
 <body>
     <div class="container">
         <div class="header">
-            <img src="{{ asset('images/logo/bezlogo.jpg') }}" alt="Bez Tower and Residences" style="pointer-events: none; user-select: none; -webkit-user-drag: none;">
+         
+            
             <h1>🏨 Check-out Reminder</h1>
             <p>Your stay is coming to an end</p>
         </div>
