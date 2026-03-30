d02e4f4b1bec05313242db822e9924b3b643192e
Lee Torres <grafrafraftorres28@gmail.com>
2026-03-30 13:16:31 +0800

    Update index.blade.php

--- resources/views/customer/rooms/index.blade.php
+++ resources/views/customer/rooms/index.blade.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>Our Rooms - Beztower & Residences</title>
     <link rel="icon" type="image/png" sizes="192x192" href="{{ asset('favicon-192x192.png') }}">
     <link rel="shortcut icon" href="{{ asset('favicon-192x192.png') }}">
